71d983da5a7f1272a4edd79cf3a1fb88baad9668
Zeroox <>
2021-02-18 03:38:20 +0500

    Fix snackbar button position

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -1,7 +1,11 @@
 <template>
   <v-snackbar v-model="show" :color="color" left bottom>
     {{ message }}
-    <v-btn text @click="show = false">Close</v-btn>
+    <template v-slot:action="{ attrs }">
+      <v-btn text v-bind="attrs" @click="show = false">
+        Close
+      </v-btn>
+    </template>
   </v-snackbar>
 </template>
 
